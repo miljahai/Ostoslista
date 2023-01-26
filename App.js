96b7391a01bc9a2165f6388abed6ad815cb3f94c
unknown <milja.haiko@gmail.com>
2023-01-26 11:05:42 +0200

    tehtävä valmis

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [data, setData] = useState([]);
+
+  const buttonPressed = () => {
+    setData([...data, {key:text}]);
+    setText('');
+  }
+
+  const clear = () => {
+    setData([]);
+    setText('');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput style={styles.input} onChangeText={text => setText(text)} value= {text}/>
+      <View style={styles.operators}>
+        <View style={styles.button}>
+          <Button title='Add' onPress={buttonPressed}/>
+        </View>
+        <View style={styles.button}>
+          <Button title='Clear' onPress={clear}/>
+        </View>
+      </View>
+      <FlatList style={styles.list} data={data} renderItem={({item}) => 
+      <Text>{item.key}</Text>} keyExtractor={(item, index) => index.toString()} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +40,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    width:200  , 
+    borderColor: 'black', 
+    borderWidth: 2,
+    marginTop: 200
+  },
+  operators: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  list: {
+
+  },
+  button: {
+    margin: 10,
   },
 });
